4c57ba0d81fad14a6539000c4a08d66010fe5a88
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-23 18:40:10 -0500

    created database calls

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,29 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "mhodges",
+    // Your MySQL password
+    password: "mjh1",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 // Default response for any other request (Not Found) this always needs to be the LAST route.
 app.use((req, res) => {
   res.status(404).end();
 });
 
+// the app.listen will always go at the bottom!!!!
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
